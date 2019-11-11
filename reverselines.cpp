9595ebc317134f385b3f69b5731c5324d717103b
Julio <JulioToboso@gmail.com>
2019-11-11 13:18:46 +0100

    Create reverselines.cpp

---
+++ reverselines.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <string>
+
+#include <cstdlib>   // The C standard header stdlib.h
+
+#include <algorithm>
+
+using namespace std;
+
+
+int main( )
+{
+    string line;
+
+    // Read the entire input at the standard input device and store in 'text'.
+    while( getline( cin, line ) ) {
+        reverse( line.begin( ), line.end( ) );
+        cout << line << "\n";
+    }
+
+    return 0;
+}
