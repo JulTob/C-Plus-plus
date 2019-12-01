0cdf0eceb704ce879e77a6a4bff5960dc490fa7e
Julio <JulioToboso@gmail.com>
2019-12-01 03:52:13 +0100

    Create hw.cpp

---
+++ HelloWorlds/hw.cpp
@@ -0,0 +1,31 @@
+//* Preprocessor Directives *//
+#include <iostream>
+  /*  #  : Preprocessor Directive Call */
+  /*  include  : Include the Library   */
+  /*  iostream : the Library           */
+  /*  <...>  : Look where the
+      standard libraries are kept      */
+  /*  "..."  : Look in the current
+      directory, then look where the
+      standard libraries are kept      */
+
+//* Main Function *//
+int main() {
+  std::cout << "Hello World" << '\n';
+  /* code */
+  return 0;
+    /* 0 : Compiled without errors */
+}
+
+
+/* To Compile:
+g++ main.cpp -o main.out
+*/
+
+/* To Run:
+./main.out
+*/
+
+/* To Compile and Run:
+g++ main.cpp -o main.out; ./main.out
+*/
