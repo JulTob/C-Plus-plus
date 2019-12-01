40de2b8bbe2adb421686c31a740a174be792d596
Julio <JulioToboso@gmail.com>
2019-12-01 04:08:47 +0100

    Create type_sizes.cpp

---
+++ basics/type_sizes.cpp
@@ -0,0 +1,20 @@
+//* Preprocessor Directives *//
+#include <iostream>
+using namespace std;
+
+//* Main Function *//
+int main() {
+  //* code *//
+
+  //sizeof(variable type)
+
+  cout << "Integer: " << sizeof(int)<< endl;
+  cout << "Short: " << sizeof(short)<< endl;
+  cout << "Long: " << sizeof(long)<< endl;
+  cout << "Char: " << sizeof(char)<< endl;
+  cout << "Float: " << sizeof(float)<< endl;
+  cout << "Double: " << sizeof(double)<< endl;
+  cout << "Booleano: " << sizeof(bool)<< endl;
+
+  return 0;
+}
